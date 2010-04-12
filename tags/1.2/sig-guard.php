17dae13058b8a12a312945b64ae20a1aea6b0309
shinephp <shinephp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-12 14:13:14 +0000

    tag 1.2 version
    git-svn-id: https://plugins.svn.wordpress.org/silence-is-golden-guard@227877 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.2/sig-guard.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Plugin Name: Silence Is Golden Guard
+Plugin URI: http://www.shinephp.com/silence-is-golden-guard-wordpress-plugin/
+Description: It prevents your blog directories from full file listing if visitor types just directory name as the URL, e.g. http://yourdomain/wp-content/plugins/
+Version: 1.2
+Author: Vladimir Garagulya
+Author URI: http://www.shinephp.com
+Text Domain: sig-guard
+Domain Path: /lang/
+*/
+
+/*
+Copyright 2009  Vladimir Garagulya  (email: vladimir@shinephp.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+if (!function_exists("get_option")) {
+  die;  // Silence is golden, direct call is prohibited
+}
+
+global $wp_version;
+
+$exit_msg = __('Silence is Golden Guard requires WordPress 2.8 or newer.').'<a href="http://codex.wordpress.org/Upgrading_WordPress">'.__('Please update!').'</a>';
+
+if (version_compare($wp_version,"2.8","<"))
+{
+	return ($exit_msg);
+}
+
+
+require_once('sig-guard_lib.php');
+
+load_plugin_textdomain('sig-guard','', $sig_guardPluginDirName.'/lang');
+
+
+function sig_guard_optionsPage() {
+
+  if (!current_user_can('activate_plugins')) {
+    die('action is forbidden');
+  }
+
+
+  $sig_guard_use_htaccess = get_option('sig_guard_use_htaccess');
+  $sig_guard_auto_monitor = get_option('sig_guard_auto_monitor');
+  $sig_guard_monitor_period = get_option('sig_guard_monitor_period');
+  $sig_guard_exclude_folders = get_option('sig_guard_exclude_folders');
+  $sig_guard_exclude_folders_list = get_option('sig_guard_exclude_folders_list');
+  
+?>
+
+<div class="wrap">
+  <div class="icon32" id="icon-options-general"><br/></div>
+    <h2><?php _e('Silence Is Golden Guard Plugin', 'sig-guard'); ?></h2>
+		<?php require ('sig-quard_options.php'); ?>
+  </div>
+<?php
+
+}
+// end of sig_guard_optionsPage()
+
+
+// Install plugin
+function sig_guard_install() {
+	
+  add_option('sig_guard_use_htaccess', 0);
+  add_option('sig_guard_auto_monitor', 0);
+  add_option('sig_guard_monitor_period', 2);
+  add_option('sig_guard_exclude_folders', 0);
+  add_option('sig_guard_exclude_folders_list', array());
+  add_option('sig_guard_last_check', time());
+
+  sig_guard_logEvent('SIG Guard Plugin is installed successfully.');
+}
+// end of sig_guard_install()
+
+
+function sig_guard_init() {
+
+  if(function_exists('register_setting')) {
+    register_setting('sig-quard-options', 'sig_guard_use_htaccess');
+    register_setting('sig-quard-options', 'sig_guard_auto_monitor');
+    register_setting('sig-quard-options', 'sig_guard_monitor_period');
+    register_setting('sig-quard-options', 'sig_guard_exclude_folders');
+    register_setting('sig-quard-options', 'sig_guard_exclude_folders_list');
+  }
+}
+// end of sig_guard_init()
+
+
+function sig_guard_plugin_action_links($links, $file) {
+    if ($file == plugin_basename(dirname(__FILE__).'/sig-guard.php')){
+        $settings_link = "<a href='options-general.php?page=sig-guard.php'>".__('Settings','sig-quard')."</a>";
+        array_unshift( $links, $settings_link );
+    }
+    return $links;
+}
+// end of sig_guard_plugin_action_links
+
+
+function sig_guard_plugin_row_meta($links, $file) {
+  if ($file == plugin_basename(dirname(__FILE__).'/sig-guard.php')){
+		$links[] = '<a target="_blank" href="http://www.shinephp.com/silence-is-golden-guard-wordpress-plugin/#changelog">'.__('Changelog', 'sig-guard').'</a>';
+	}
+	return $links;
+} // end of sig_guard_plugin_row_meta
+
+
+function sig_guard_settings_menu() {
+	if ( function_exists('add_options_page') ) {
+    $sig_guard_page = add_options_page('Silence Is Golden Guard', 'SIG Guard', 9, basename(__FILE__), 'sig_guard_optionsPage');
+		add_action( "admin_print_styles-$sig_guard_page", 'sig_guard_adminCssAction' );
+	}
+}
+// end of sig_guard_settings_menu()
+
+function sig_guard_adminCssAction() {
+
+  wp_enqueue_style('sig_guard_admin_css', SIG_GUARD_PLUGIN_URL.'/css/sig-guard_admin.css', array(), false, 'screen');
+
+}
+// end of sig_guard_adminCssAction()
+
+
+
+if (is_admin()) {
+  // activation action
+  register_activation_hook(__FILE__, "sig_guard_install");
+
+  add_action('admin_init', 'sig_guard_init');
+  // add a Settings link in the installed plugins page
+  add_filter('plugin_action_links', 'sig_guard_plugin_action_links', 10, 2);
+  add_filter('plugin_row_meta', 'sig_guard_plugin_row_meta', 10, 2);
+}
+
+add_action('admin_menu', 'sig_guard_settings_menu');
+
+$sig_guard_auto_monitor = get_option('sig_guard_auto_monitor');
+if ($sig_guard_auto_monitor) {
+  function sig_guard_action() {
+    $sig_guard_last_check = get_option('sig_guard_last_check');
+    if (time() - $sig_guard_last_check > 86400) { // last check was made more then 1 day ago
+      sig_guard_Scan();      
+    }
+  }
+  // end of sig_guard_action()
+
+  add_action('wp_head', 'sig_guard_action');
+}
+
+?>
---
+++ tags/1.2/sig-quard_options.php
@@ -0,0 +1,149 @@
+<?php
+/* 
+ * Silence Is Golden Guard plugin Settings form
+ * 
+ */
+
+if (!defined('SIG_GUARD_PLUGIN_URL')) {
+  die;  // Silence is golden, direct call is prohibited
+}
+
+function sig_guard_displayBoxStart($title) {
+?>
+			<div class="postbox" style="float: left;">
+				<h3 style="cursor:default;"><span><?php echo $title ?></span></h3>
+				<div class="inside">
+<?php
+}
+// 	end of thanks_displayBoxStart()
+
+function sig_guard_displayBoxEnd() {
+?>
+				</div>
+			</div>
+<?php
+}
+// end of thanks_displayBoxEnd()
+
+$mess = '';
+$shinephpFavIcon = SIG_GUARD_PLUGIN_URL.'/images/vladimir.png';
+if (isset($_GET['action']) && $_GET['action']=='scan') {
+  sig_guard_Scan(true);
+  return;
+}
+?>
+  <form method="post" action="options.php">
+<?php
+    settings_fields('sig-quard-options');
+?>
+				<div id="poststuff" class="metabox-holder has-right-sidebar">
+					<div class="inner-sidebar" >
+						<div id="side-sortables" class="meta-box-sortabless ui-sortable" style="position:relative;">
+									<?php sig_guard_displayBoxStart(__('About this Plugin:', 'sig-guard')); ?>
+											<a class="sig_guard_rsb_link" style="background-image:url(<?php echo $shinephpFavIcon; ?>);" target="_blank" href="http://www.shinephp.com/"><?php _e("Author's website", 'sig-guard'); ?></a>
+											<a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/sig-guard-icon.png'; ?>" target="_blank" href="http://www.shinephp.com/silence-is-golden-guard-wordpress-plugin/"><?php _e('Plugin webpage', 'sig-guard'); ?></a>
+											<a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/changelog-icon.png'; ?>);" target="_blank" href="http://www.shinephp.com/silence-is-golden-guard-wordpress-plugin/#changelog"><?php _e('Changelog', 'sig-guard'); ?></a>
+											<a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/faq-icon.png'; ?>)" target="_blank" href="http://www.shinephp.com/silence-is-golden-guard-wordpress-plugin/#faq"><?php _e('FAQ', 'sig-guard'); ?></a>
+                      <a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/donate-icon.png'; ?>)" target="_blank" href="http://www.shinephp.com/donate"><?php _e('Donate', 'sig-guard'); ?></a>
+									<?php sig_guard_displayBoxEnd(); ?>
+									<?php sig_guard_displayBoxStart(__('Greetings:','sig-guard')); ?>
+											<a class="sig_guard_rsb_link" style="background-image:url(<?php echo $shinephpFavIcon; ?>);" target="_blank" title="<?php _e("It's me, the author", 'sig-guard'); ?>" href="http://www.shinephp.com/">Vladimir</a>
+                      <a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/whiler.png'; ?>);" target="_blank" title="<?php _e('for the help with French translation', 'sig-guard'); ?>" href="http://blogs.wittwer.fr/whiler/">Whiler</a>
+                      <a class="sig_guard_rsb_link" style="background-image:url(<?php echo SIG_GUARD_PLUGIN_URL.'/images/omi.png'; ?>);" target="_blank" title="<?php _e('for the help with Spanish translation', 'thankyou'); ?>" href="http://equipajedemano.info/">Omi</a>
+											<?php _e('Do you wish to see your name with link to your site here? You are welcome! Your help with translation and new ideas are very appreciated.', 'sig-guard'); ?>
+									<?php sig_guard_displayBoxEnd(); ?>
+						</div>
+					</div>
+					<div class="has-sidebar" >
+						<div id="post-body-content" class="has-sidebar-content">
+<script language="javascript" type="text/javascript">
+  function sig_guard_hideShowDiv(checkbox) {
+    var el = document.getElementById('foldersdiv')
+    if (checkbox.checked) {
+      el.style.display = 'block';
+    } else {
+      el.style.display = 'none';
+    }
+  }
+
+  function sig_guard_Settings(action) {
+    if (action=='cancel') {
+      document.location = '<?php echo SIG_GUARD_WP_ADMIN_URL; ?>/options-general.php?page=sig-guard.php';
+    } else if (action=='scan') {
+      document.location = '<?php echo SIG_GUARD_WP_ADMIN_URL; ?>/options-general.php?page=sig-guard.php&action=scan';
+    }
+  }
+
+</script>
+<?php
+						sig_guard_displayBoxStart(__('Settings', 'sig-guard')); ?>
+        <table class="form-table" style="clear:none;" cellpadding="0" cellspacing="0">          
+          <tr>
+            <td style="vertical-align:top;width:200px;">
+              <input type="checkbox" value="1" <?php echo ($sig_guard_exclude_folders=='1') ? 'checked="checked"' : ''; ?>
+                       name="sig_guard_exclude_folders" id="sig_guard_exclude_folders" onclick="sig_guard_hideShowDiv(this)"
+                       title="<?php _e('Does not create index.php file in the checked folders'); ?>"/>
+	             <?php _e('Exclude Folders for index.php','sig-guard'); ?>
+            </td>
+            <td>                      
+<?php 
+  $folders = sig_guard_getBlogFolders();
+
+?>
+                <div id="foldersdiv" style="display:<?php echo ($sig_guard_exclude_folders) ? 'block':'none';?>">
+                  <div id="folders-all" class="tabs-panel">
+                  <ul>
+<?php
+  
+  foreach ($folders as $folder) {
+    $checked = '';
+    if (is_array($sig_guard_exclude_folders_list) and count($sig_guard_exclude_folders_list)) {
+      foreach($sig_guard_exclude_folders_list as $folderToExclude) {
+        if ($folderToExclude==$folder) {
+          $checked = 'checked="checked"';
+          break;
+        }
+      }
+    }
+?>
+   <li><input type="checkbox" name="sig_guard_exclude_folders_list[]" value="<?php echo $folder;?>" <?php echo $checked; ?> />
+<?php echo $folder; ?>
+   </li>
+<?php    
+  }
+?>
+                  </ul>
+                  </div>
+                </div>                
+            </td>
+          </tr>
+          <tr>
+          <td style="vertical-align: top;">
+            <input type="checkbox" name="sig_guard_use_htaccess" id="sig_guard_use_htaccess" value="1" <?php echo sig_guard_optionChecked($sig_guard_use_htaccess, 1); ?> />
+            <?php _e('Modify Apache .htaccess', 'sig-guard'); ?>
+          </td>
+          <td>
+            <?php _e('Modify Apache .htaccess file in the site root folder. Add "Options -Indexes" line to prevent directory listing by Apache Web server.
+                      If it is turned on (+Indexes) or absent, then if a URL which maps to a directory is requested, and there is no DirectoryIndex (e.g., index.html or index.php) file in that directory, then Web server will return a formatted listing of the directory.', 'sig-guard')?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" name="sig_guard_auto_monitor" id="sig_guard_auto_monitor" value="1" <?php echo sig_guard_optionChecked($sig_guard_auto_monitor, 1); ?>
+                   title="<?php _e('Check folders state automatically with specified period'); ?>"/>
+            <label for="sig_guard_auto_monitor"><?php _e('Auto Monitor','sig-guard'); ?></label>
+          </td>
+          <td class="submit">
+            <input type="button" name="scanNow" value="<?php _e('Scan Now', 'sig-guard') ?>" title="<?php _e('Scan and Fix directories listing related problems Now','sig-guard');?>" onclick="sig_guard_Settings('scan');"/>
+          </td>
+        </tr>
+      </table>
+			<?php sig_guard_displayBoxEnd();?>
+      <div class="fli submit" style="padding-top: 0px;">
+          <input type="submit" name="submit" value="<?php _e('Update', 'sig-guard'); ?>" title="<?php _e('Save Changes', 'sig-guard'); ?>" />
+          <input type="button" name="cancel" value="<?php _e('Cancel', 'sig-guard') ?>" title="<?php _e('Cancel not saved changes','sig-guard');?>" onclick="sig_guard_Settings('cancel');"/>
+      </div>
+						</div>
+					</div>
+				</div>
+    </form>
